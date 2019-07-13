7af37d212e02e5c688ada91ac4a45c94962896cf
chankruze <chankruze@gmail.com>
2019-07-13 20:51:17 +0530

    Create mining application & API
    * create an express web server
    * create an api with GET & POST endpoints
    
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain');
+
+//$ HTTP_PORT=3002 npm run dev
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new Blockchain();
+
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added ${block.toString()}`);
+
+    res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
