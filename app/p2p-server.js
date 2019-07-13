593daa40bba9695409c96a0e2cc5711cb9d148e5
chankruze <chankruze@gmail.com>
2019-07-13 22:04:37 +0530

    implements sending & receiving messages between peers
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -22,6 +22,10 @@ class P2pServer {
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
     }
 
     connectToPeers() {
@@ -30,6 +34,13 @@ class P2pServer {
             const socket = new Websocket(peer);
 
             socket.on('open', () => this.connectSocket(socket));
+        });
+    }
+
+    messageHandler(socket){
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data);
         });
     }
 }
