049f8dfc1d2ad1ac1e24d4380d6f7c523d920179
chankruze <chankruze@gmail.com>
2019-07-13 21:04:50 +0530

    Create p2p network | Implements server creation
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/p2p-server.js
@@ -0,0 +1,23 @@
+const Websocket = require('ws');
+
+//$ HTTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer {
+    constructor(blockchain) {
+        this.blockchain = blockchain;
+        this.sockets = [];
+    }
+
+    listen() {
+        const server = new Websocket.Server({ port: P2P_PORT });
+        server.on('connection', socket => this.connectSocket(socket));
+        console.log(`Listening for peer-to-peer on: ${P2P_PORT}`);
+    }
+
+    connectSocket(socket){
+        this.sockets.push(socket);
+        console.log('Socket connected');
+    }
+}
