f1754ee057964e758091dc5b0d9e3d52bc1335ed
chankruze <chankruze@gmail.com>
2019-07-13 21:09:20 +0530

    implements peer connecting to existing network
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 //$ HTTP_PORT=3002 npm run dev
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -22,3 +24,4 @@ app.post('/mine', (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -13,11 +13,25 @@ class P2pServer {
     listen() {
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on('connection', socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
         console.log(`Listening for peer-to-peer on: ${P2P_PORT}`);
     }
 
-    connectSocket(socket){
+    connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
     }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            // ws://localhost:5001
+            const socket = new Websocket(peer);
+
+            socket.on('open', () => this.connectSocket(socket));
+        });
+    }
 }
+
+module.exports = P2pServer;
