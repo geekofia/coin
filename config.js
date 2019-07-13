771ac8d73f3c7dae42f77b7eb05f17b9dabf1bc7
chankruze <chankruze@gmail.com>
2019-07-14 02:11:29 +0530

    Create Wallet Class
    * create just the skelton of indivisual wallet
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- config.js
+++ config.js
@@ -3,5 +3,6 @@
  *******************/
 const DIFFICULTY = 4;
 const MINE_RATE = 3000;
+const INITIAL_BALANCE = 500;
 
-module.exports = { DIFFICULTY, MINE_RATE };
+module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE};
