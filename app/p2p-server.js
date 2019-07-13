50c9742e6a15501c78cf04c79161619ea0d0c988
chankruze <chankruze@gmail.com>
2019-07-13 22:31:32 +0530

    Add chain syncronization
    * Broadcasting each peer chain is now moved to a helper function
    * The chain of each peer is now syncronized with the longest chain
    * Small chain of new peers replace by and sync with the longest chain present in the network & their small chain broadcast is rejected by longest chain
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -25,7 +25,7 @@ class P2pServer {
 
         this.messageHandler(socket);
 
-        socket.send(JSON.stringify(this.blockchain.chain));
+        this.sendChain(socket);
     }
 
     connectToPeers() {
@@ -40,8 +40,17 @@ class P2pServer {
     messageHandler(socket){
         socket.on('message', message => {
             const data = JSON.parse(message);
-            console.log('data', data);
+
+            this.blockchain.replaceChain(data);
         });
+    }
+
+    sendChain(socket){
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    syncChains(){
+        this.sockets.forEach(socket => this.sendChain(socket));
     }
 }
 
