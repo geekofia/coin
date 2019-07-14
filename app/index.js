8c7f56872c68dff2948db174c686433a33223dfd
chankruze <chankruze@gmail.com>
2019-07-14 16:53:35 +0530

    Add POST & GET transaction endpoint to application (local)
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/index.js
+++ app/index.js
@@ -2,19 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 //$ HTTP_PORT=3002 npm run dev
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
+/** 
+ * GET endpoints
+*/
+
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+/** 
+ * POST endpoints
+*/
 
 app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
@@ -23,6 +39,13 @@ app.post('/mine', (req, res) => {
     p2pServer.syncChains();
 
     res.redirect('/blocks');
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+
+    res.redirect('/transactions');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
