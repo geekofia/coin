7d7e3201c6c42000b8a1eed806e1f66e256a6b26
chankruze <chankruze@gmail.com>
2019-07-16 00:21:48 +0530

    Add dev-test for blocks
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ dev-tests/dev-test-block.js
@@ -0,0 +1,7 @@
+const Block = require('../blockchain/block');
+
+// Create a new instance of Block
+const testBlock = Block.mineBlock(Block.genesis(), 'chankruze gave $10,000 to nishant');
+
+// Log the test data
+console.log(testBlock.toString());
